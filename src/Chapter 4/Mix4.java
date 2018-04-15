0c94bbdb52e8455d35b71ad5269c4e46365333da
Lucus Lally <lucuslally1@gmail.com>
2018-04-14 18:26:11 -0700

    Added Mix4 class

---
+++ src/Chapter 4/Mix4.java
@@ -0,0 +1,40 @@
+public class Mix4 {
+    int counter = 0;
+    public static void main(String[] args){
+        int count = 0;
+        Mix4[] m4a = new Mix4[20];
+        int x = 0;
+
+        while(x < 7){
+            m4a[x] = new Mix4();
+            m4a[x].counter = m4a[x].counter + 1;
+            count = count +1;
+            count = count + m4a[x].maybeNew(x);
+            x = x +1;
+        }
+        System.out.println(count + " " + m4a[1].counter);
+    }
+    public int maybeNew(int index){
+        if(index < 7){
+            Mix4 m4 = new Mix4();
+            m4.counter = m4.counter + 1;
+            return 1;
+        }
+        return 0;
+    }
+}
+
+/*
+x < 9
+index < 5 = 14 1
+
+x < 20
+index < 5 = 25 1
+
+x < 7
+index < 7 = 14 1
+
+x < 19
+index < 1 = 20 1
+
+ */
