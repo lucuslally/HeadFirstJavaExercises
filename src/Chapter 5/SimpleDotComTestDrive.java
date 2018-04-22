b11994da0b3ecbaaba440eff67e61760e6e4bb0d
Lucus Lally <lucuslally1@gmail.com>
2018-04-21 21:48:23 -0700

    Added SimpleDotComTestDrive class

---
+++ src/Chapter 5/SimpleDotComTestDrive.java
@@ -0,0 +1,47 @@
+public class SimpleDotComTestDrive{
+    public static void main (String[] args){
+        SimpleDotCom dot = new SimpleDotCom();
+        int [] locations = {2,3,4};
+        dot.setLocationCells(locations);
+        String userGuess = "2";
+        String result = dot.checkYourself(userGuess);
+    }
+}
+
+class SimpleDotCom {
+    int[] locationCells;
+    int numOfHits = 0;
+
+    void setLocationCells(int[] locs) {
+        // ASSIGN the cell locations parameter to the cell locations instance variable
+        locationCells = locs;
+
+    }
+
+    public String checkYourself(String stringGuess) {
+        // CONVERT users guess to an int
+        int guess = Integer.parseInt(stringGuess);
+
+        String result = "miss";
+
+        // REPEAT with each of the location cells in the int array
+        for (int cell : locationCells) {
+            // COMPARE the user guess to the location cell
+            if (guess == cell) { // IF the user guesses the matches
+                result = "hit";
+                numOfHits++; // INCREMENT the number of hits
+                break;
+            }
+
+        }
+
+        // find out if it was the last location cell
+        if (numOfHits == locationCells.length) {  // IF number of hits are 3, return "kill" as the results
+            result = "kill";
+        }
+
+        System.out.println(result);
+        return result;
+
+    }
+}
