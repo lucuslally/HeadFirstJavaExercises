61db23dcd129583bb8d0950ab38d2e3ecaf7f01a
Lucus Lally <lucuslally1@gmail.com>
2018-04-22 20:55:12 -0700

    Adding MixFor5 class

---
+++ src/Chapter 5/MixFor5.java
@@ -0,0 +1,27 @@
+public class MixFor5 {
+    public static void main(String[] args){
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer <3; outer++){
+            for (int inner = 4; inner >1; inner--){
+                // candidate code
+                x = x + 0;
+
+                y = y - 2;
+                if ( x == 6){
+                    break;
+                }
+                x = x + 3;
+
+            }
+            y = y - 2;
+        }
+        System.out.println(x + " " + y);
+    }
+}
+
+// x = x + 3; 54 6
+// x = x + 6; 60 10
+// x++; 36 6
+// x--; 18 6
+// x = x + 0; 6 14
