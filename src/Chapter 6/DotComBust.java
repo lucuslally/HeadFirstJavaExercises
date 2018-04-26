33123ae7ae186eda981d162bb86eefcf6aa78736
Lucus Lally <lucuslally1@gmail.com>
2018-04-25 19:21:03 -0700

    Progressing on DotComBust class

--- src/Chapter 6/DotComBust.java
+++ src/Chapter 6/DotComBust.java
@@ -1,2 +1,49 @@
+import java.lang.reflect.Array;
+import java.util.*;
 public class DotComBust {
+
+    // declare and initialize the variables we'll need
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame(){
+
+        // first make some dot coms and give them locations
+        // make three DotCom objects, give 'em names, and stick 'em in the ArrayList
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        // print brief instructions to the user
+        System.out.println("your goal is to sink three dot coms.");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Try to sink them all in the fewest number of guesses");
+
+        // repeat with each DotCom in the list
+        for (DotCom dotComToSet : dotComsList){
+
+            // as the helper for a DotCom location
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+
+            // call the setter method on this DotCom to give it the location you just got from the helper
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+
+    private void startPlaying(){
+
+        // as long as the DotCom list is NOT empty
+        while(!dotComsList.isEmpty()){
+            String userGuess = helper.getUserInput("Enter a Guess");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
 }
